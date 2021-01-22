a6095b09491f36e85593f77860336a9e0d846bb5
ALI <1416978277@qq.com>
2021-01-22 21:07:11 +0800

    0122

--- src/main/java/com/demo/Test.java
+++ src/main/java/com/demo/Test.java
@@ -1,20 +1,21 @@
 package com.demo;
 
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import com.demo.constant.CaptchaTypeEnum;
 import com.demo.constant.ResultCodeEnum;
+import com.demo.entity.po.RoleApiTree;
 import com.demo.entity.po.User;
 import com.demo.entity.pojo.Result;
 import com.demo.entity.pojo.ResultBatch;
 import com.demo.entity.vo.RoleApiTreeVo;
 import com.demo.entity.vo.UserVo;
-
 import eu.bitwalker.useragentutils.UserAgent;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * <h1>测试类</h1>
@@ -66,19 +67,26 @@ public class Test {
         System.out.println(batchResult.isOk());
 
         // google chrome
-//        String userAgentString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36";
+        //        String userAgentString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like
+        //        Gecko) Chrome/72.0.3626.119 Safari/537.36";
         // ie9
         String userAgentString = "User-Agent:Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;";
         // safari
-//        String userAgentString = "User-Agent:Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50";
+        //        String userAgentString = "User-Agent:Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us)
+        //        AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50";
         // iphone
-//        String userAgentString = "User-Agent:Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5";
+        //        String userAgentString = "User-Agent:Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X;
+        //        en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5";
         // ipad
-//        String userAgentString = "User-Agent:Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5";
+        //        String userAgentString = "User-Agent:Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us)
+        //        AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5";
         // android
-//        String userAgentString = "User-Agent: MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1";
+        //        String userAgentString = "User-Agent: MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn;
+        //        MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile
+        //        Safari/533.1";
         // windows phone
-//        String userAgentString = "User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0; HTC; Titan)";
+        //        String userAgentString = "User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5;
+        //        Trident/5.0; IEMobile/9.0; HTC; Titan)";
         UserAgent userAgent = new UserAgent(userAgentString);
         System.out.println(userAgent.getBrowser().getName());
         System.out.println(userAgent.getBrowserVersion());
@@ -86,23 +94,6 @@ public class Test {
         System.out.println(userAgent.getOperatingSystem().getDeviceType().getName());
 
         System.out.println(CaptchaTypeEnum.findByType(1));
-
-        String a1 = "Ab";
-        String a2 = null;
-        System.out.println(a1.equalsIgnoreCase(a2));
-
-        Object o = "男";
-        System.out.println(o);
-        System.out.println(o.toString());
-        System.out.println((String) o);
-//        System.out.println((Integer) o);
-//        System.out.println(Integer.valueOf((String) o));
-//        System.out.println(Integer.valueOf(o.toString()));
-        System.out.println("男".equals(o));
-
-        String s = "1234";
-        Long l = 1234L;
-        System.out.println(s.equals(l.toString()));
 
         List<RoleApiTreeVo> list = new ArrayList<>();
         list.add(new RoleApiTreeVo(0L, 0L, ""));
@@ -121,75 +112,96 @@ public class Test {
         System.out.println(list);
         RoleApiTreeVo tree = list2Tree(list);
         System.out.println(tree);
-        List<RoleApiTreeVo> aa = new ArrayList<>();
-        tree2ExpandedList(aa, tree, "/", "");
-        System.out.println(aa);
+        List<RoleApiTreeVo> bb = tree2ExpandedList(tree);
+        System.out.println(bb);
+
     }
 
     /**
      * 列表转树
-     * 
+     *
      * @param list 列表
      */
     public static RoleApiTreeVo list2Tree(List<RoleApiTreeVo> list) {
         // 按parentId分组
         Map<Long, List<RoleApiTreeVo>> map = list.stream().collect(Collectors.groupingBy(RoleApiTreeVo::getParentId));
         // 找到根节点，id和parentId都为0
-        RoleApiTreeVo root = map.get(0L).stream().filter(s -> s.getId() == 0L).findFirst().get();
+        RoleApiTreeVo tree = map.get(0L).stream().filter(s -> s.getId() == 0L).findFirst().get();
         // 把根节点从map中去除，防止循环嵌套
-        map.get(0L).remove(root);
+        map.get(0L).remove(tree);
         // 生成树
-        makeTree(root, map);
-        return root;
+        makeTree(tree, map);
+        return tree;
     }
 
     /**
      * 生成树
-     * 
-     * @param root 树的根节点
-     * @param map  按parentId分组并且去除根节点的列表
+     *
+     * @param tree 接收的树
+     * @param map  按parentId分组并且去除根节点的map
      */
-    public static void makeTree(RoleApiTreeVo root, Map<Long, List<RoleApiTreeVo>> map) {
+    public static void makeTree(RoleApiTreeVo tree, Map<Long, List<RoleApiTreeVo>> map) {
         // 找到子节点
-        List<RoleApiTreeVo> childs = map.get(root.getId());
+        List<RoleApiTreeVo> childs = map.get(tree.getId());
         if (childs != null) {
             // 子节点生成树
-            for (RoleApiTreeVo chile : childs) {
-                makeTree(chile, map);
+            for (RoleApiTreeVo child : childs) {
+                makeTree(child, map);
             }
             // 插入子节点
-            root.setChilds(childs);
+            tree.setChilds(childs);
         }
     }
 
     /**
-     * 树转展开的列表
-     * 
+     * 树转展开列表
+     *
      * @param tree 树
      */
-    public static void tree2ExpandedList(List<RoleApiTreeVo> list, RoleApiTreeVo tree, String prefix,
-            String newPrefix) {
-        RoleApiTreeVo root = tree;
-        newPrefix = newPrefix + prefix + root.getPath();
-        root.setPath(newPrefix);
-        List<RoleApiTreeVo> childs = root.getChilds();
+    public static List<RoleApiTreeVo> tree2ExpandedList(RoleApiTreeVo tree) {
+        List<RoleApiTreeVo> list = new ArrayList<>();
+        // 设置根节点的path
+        tree.setPath("/");
+        // 找到子节点
+        List<RoleApiTreeVo> childs = tree.getChilds();
+        // 子节点去展开
         if (childs != null) {
             for (RoleApiTreeVo child : childs) {
-                tree2ExpandedList(list, child, prefix, newPrefix);
+                makeExpandedList(list, child, "/", "");
             }
         }
-        root.setChilds(null);
-        list.add(root);
+        // 子节点清空
+        tree.setChilds(null);
+        // 插入根节点
+        list.add(tree);
+        // 所有的节点展开完成后根据path排序
+        return list.stream().sorted(Comparator.comparing(RoleApiTree::getPath)).collect(Collectors.toList());
     }
 
     /**
-     * 建造扁平的树
-     * 
-     * @param tree 树
+     * 生成展开列表
+     *
+     * @param list      接收的列表
+     * @param tree      树
+     * @param prefix    前缀
+     * @param newPrefix 新的前缀
      */
-    public static List<RoleApiTreeVo> buildFlattenedTree(RoleApiTreeVo tree, String prefix) {
-
-        return null;
+    public static void makeExpandedList(List<RoleApiTreeVo> list, RoleApiTreeVo tree, String prefix, String newPrefix) {
+        // 节点设置新的前缀
+        newPrefix = newPrefix + prefix + tree.getPath();
+        tree.setPath(newPrefix);
+        // 找到子节点
+        List<RoleApiTreeVo> childs = tree.getChilds();
+        // 子节点去展开
+        if (childs != null) {
+            for (RoleApiTreeVo child : childs) {
+                makeExpandedList(list, child, prefix, newPrefix);
+            }
+        }
+        // 子节点清空
+        tree.setChilds(null);
+        // 插入节点
+        list.add(tree);
     }
 
 }
